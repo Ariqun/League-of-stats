f3724b0598db276945333ebfa3bd479a45ac9b6d
Ariqun <myhomewalhall@gmail.com>
2021-04-16 14:25:38 +0300

    Styled general info blok of champ, v0.0.7

--- Client/src/components/champComponents/champGeneral.js
+++ Client/src/components/champComponents/champGeneral.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react'
 
 export default class ChampGeneral extends Component {
-	
-
-	
-
 	render() {
 		const {lore, stats} = this.props.info
-		console.log(stats)
-		// CreateTable(stats)
+
 		return(
 			<div className="general">
 				<div className="lore">
-					<span>{lore}</span>
+					<p>{lore}</p>
 				</div>
 
 				<div className="stats">
@@ -25,33 +20,52 @@ export default class ChampGeneral extends Component {
 
 const CreateTable = ({stats}) => {
 	const ruObj = {
-		armor: 'Броня', armorperlevel: 'Броня за уровень', attackdamage: 'Сила атаки', attackdamageperlevel: 'Сила атаки за уровень', attackrange: 'Радиус атаки', attackspeed: 'Скорость атаки', attackspeedperlevel: 'Скорость атаки за уровень', crit: 'Шанс крита', critperlevel: 'Шанс крита за уровень', hp: 'Здоровье', hpperlevel: 'Здоровье за уровень', hpregen: 'Восстановление здоровья', hpregenperlevel: 'Восстановление здоровья за уровень', movespeed: 'Скорость', mp: 'Ресурс', mpperlevel: 'Ресурс за уровень', mpregen: 'Восстановление ресурса', mpregenperlevel: 'Восстановление ресурса за уровень', spellblock: 'Сопротивление магии', spellblockperlevel: 'Сопротивление магии за уровень'
+		armor: 'Броня', armorperlevel: 'Броня за уровень', attackdamage: 'Сила атаки', attackdamageperlevel: 'Сила атаки за уровень', attackspeed: 'Скорость атаки', attackspeedperlevel: 'Скорость атаки за уровень', crit: 'Шанс крита', critperlevel: 'Шанс крита за уровень', hp: 'Здоровье', hpperlevel: 'Здоровье за уровень', hpregen: 'Восстановление здоровья', hpregenperlevel: 'Восстановление здоровья за уровень', mp: 'Ресурс', mpperlevel: 'Ресурс за уровень', mpregen: 'Восстановление ресурса', mpregenperlevel: 'Восстановление ресурса за уровень', spellblock: 'Сопротивление магии', spellblockperlevel: 'Сопротивление магии за уровень',  attackrange: 'Радиус атаки', movespeed: 'Скорость'
 	}
 	const arr = [];
 
-	for (let key in stats) {
-		let ruName = '';
-		
-		if (ruObj.hasOwnProperty(key)) {
-			ruName = ruObj[key]
+	const sortArr = () => {
+		// Такая наркомания нужна для правильного формирования окончательной таблицы
+		for (let key in stats) {
+			if (ruObj.hasOwnProperty(key) && (key !== 'attackrange' && key !== 'movespeed' && key !== 'attackspeedperlevel')) {
+				arr.push(`${ruObj[key]}: ${stats[key]}`)
+			}
 		}
 
-		arr.push(`${ruName}: ${stats[key]}`)
+		for (let key in stats) {
+			if (ruObj.hasOwnProperty(key) && (key === 'attackspeedperlevel')) {
+				arr.push(`${ruObj[key]}: ${stats[key]}`)
+			}
+		}
+
+		for (let key in stats) {
+			if (ruObj.hasOwnProperty(key) && (key === 'attackrange' || key === 'movespeed')) {
+				arr.push(`${ruObj[key]}: ${stats[key]}`)
+			}
+		}
 	}
+	sortArr()
 
 	const table = arr.map((item, i) => {
 		const name = item.split(':')[0]
 		const value = item.split(':')[1]
 
-		return(
-			<div className="stat" key={i}>
-				<span className="stat_name">{name}</span>
-				<span className="stat_value">{value}</span>
-			</div>
-		)
+		if (i % 2 === 0) {
+			return(
+				<div className="stat" key={i}>
+					<span className="stat_name">{name} - </span>
+					<span className="stat_value">{value}</span>
+				</div>
+			)
+		} else {
+			return(
+				<div className="stat" key={i}>
+					<span className="stat_value">{value}</span>
+					<span className="stat_name"> - {name}</span>
+				</div>
+			)
+		}
 	})
 
-	return(
-		table
-	)
+	return(table)
 }
